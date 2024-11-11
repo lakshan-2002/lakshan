e731a14e73619488e351d82ad4d5c8ea9ff9a8df
lakshan-2002 <lakshan.imantha02@gmail.com>
2024-11-11 19:22:39 +0530

    version 3

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(){
-     cout << "Hello World!" << endl;
      cout << "Hello Lakshan!" << endl;
+     cout << "I love programming." << endl;
      return 0;
 }
+
 
 
